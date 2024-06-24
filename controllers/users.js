0f035965ca62c437b2f05ae524ff7829120a13c3
Brittany Nicole <bnsquires1@gmail.com>
2024-06-24 03:18:11 -0400

    user routes and controllers

--- controllers/users.js
+++ controllers/users.js
@@ -2,6 +2,8 @@ const User = require('../models/Company');
 
 module.exports = {
   createUser,
+  getUsers,
+  deleteUser,
 };
 
 async function createUser(req, res) {
@@ -12,4 +14,26 @@ async function createUser(req, res) {
   } catch (err) {
     res.status(400).json(err);
   }
-}
+};
+
+//get all users
+async function getUsers(req, res) {
+  try {
+    const users = await User.find({})
+
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(400).send(err)
+  }
+};
+
+//delete a user by ID
+async function deleteUser(req, res) {
+  try {
+    const users = await User.findByIdAndDelete(req.params.id)
+
+    res.status(200).json(deleteUser);
+  } catch (err) {
+    res.status(400).send(err)
+  }
+};
--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,11 @@ const usersCtrl = require('../controllers/users.js');
 
 const router = Router();
 
-
+//Create a user
+router.post('/', usersCtrl.createUser);
+//Read all users
+router.get('/', usersCtrl.getUsers);
+//delete a user by ID
+router.delete('/:id', usersCtrl.deleteUser);
 
 module.exports = router;
